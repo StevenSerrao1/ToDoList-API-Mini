47e57871cf46bd4b999676bfff2b7e490eed6c4c
Steven Serrao <stevenserraowork@gmail.com>
2025-11-23 07:34:46 +0200

    initialization of project template via vs2022

---
+++ ToDoList-API-Mini/Controllers/ToDoListController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList_API_Mini.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoListController : ControllerBase
+    {
+        
+    }
+}
